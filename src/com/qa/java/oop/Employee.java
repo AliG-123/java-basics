c36b388ba07fb487eba07d2047582669652dc36f
Ali <aligoni123@hotmail.com>
2022-09-28 14:02:25 +0100

    Contructors

--- src/com/qa/java/oop/Employee.java
+++ src/com/qa/java/oop/Employee.java
@@ -12,4 +12,27 @@ public class Employee {
 	char gender;
 	//Each employee object will have its own state
 
+	//No-arg constructor
+	Employee(){
+		System.out.println("Employee() constructor is called ..");
+		id = 111;
+		name = "emp1";
+		department = "Develeopment";
+		salary = 12400;
+		age = 25;
+		gender = 'M';
+				
+	//Arg Constructor
+				
+	}
+
+	public Employee(int id, String name, String department, double salary, byte age, char gender) {
+		System.out.println("Employee(arg) comstructor is called");;
+		this.id = id;
+		this.name = name;
+		this.department = department;
+		this.salary = salary;
+		this.age = age;
+		this.gender = gender;
+	}
 }
--- src/com/qa/java/oop/Main.java
+++ src/com/qa/java/oop/Main.java
@@ -6,6 +6,20 @@ public class Main {
 				
 				Employee employee; // a reference variable		
 				employee = new Employee(); // an employee object is created
+//				System.out.println("id: "+ employee.id);
+//				System.out.println("name:" +employee.name);
+//				System.out.println("department:" +employee.department);
+//				System.out.println("gender:" + employee.gender);
+//				System.out.println("salary:" + employee.salary);
+//				System.out.println("age:" + employee.age);
+				
+//				employee.id = 111;
+//				employee.name = "emp1";
+//				employee.department = "development";
+//				employee.gender = 'M';
+//				employee.salary = 46564.45;
+//				employee.age = 25;
+				
 				System.out.println("id: "+ employee.id);
 				System.out.println("name:" +employee.name);
 				System.out.println("department:" +employee.department);
@@ -13,27 +27,10 @@ public class Main {
 				System.out.println("salary:" + employee.salary);
 				System.out.println("age:" + employee.age);
 				
-				employee.id = 111;
-				employee.name = "emp1";
-				employee.department = "development";
-				employee.gender = 'M';
-				employee.salary = 46564.45;
-				employee.age = 25;
+				System.out.println();
 				
-				System.out.println("id: "+ employee.id);
-				System.out.println("name:" +employee.name);
-				System.out.println("department:" +employee.department);
-				System.out.println("gender:" + employee.gender);
-				System.out.println("salary:" + employee.salary);
-				System.out.println("age:" + employee.age);
+				Employee employee2 = new Employee(222,"emp 2","Development",26564.00, (byte) 26, 'F');
 				
-				Employee employee2 = new Employee();
-				employee2.id = 222;
-				employee2.name = "emp2";
-				employee2.department = "QA";
-				employee2.gender = 'F';
-				employee2.salary = 56564.45;
-				employee2.age = 26;
 
 				System.out.println("id: "+ employee2.id);
 				System.out.println("name:" +employee2.name);
