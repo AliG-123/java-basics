fd4940b00355bbc04dd26b847422ea82a7158f3f
Ali <aligoni123@hotmail.com>
2022-09-28 14:26:43 +0100

    Constructors with method

--- src/com/qa/java/oop/Product.java
+++ src/com/qa/java/oop/Product.java
@@ -17,15 +17,7 @@ public class Product {
 		rating = 3;
 		discountPercentage = 10;
 		isAvailable = true;
-		System.out.println("Product id: "+ id);
-		System.out.println("Product name: " + name);
-		System.out.println("Product Catergory: " + category);
-		System.out.println("Product rating: " + rating + "/5.0");
-		System.out.println("Product in stock: " + isAvailable);
-		System.out.println(discountPercentage + "% discount available");
-		System.out.println("Originial Product Price: £" + price);
-		System.out.println("Discounted Product Price: £" + (price - (price * discountPercentage/100)));
-		System.out.println();
+		
 	}
 
 	public Product(int id, String name, double price, String category, double rating, int discountPercentage,
@@ -37,15 +29,7 @@ public class Product {
 		this.rating = rating;
 		this.discountPercentage = discountPercentage;
 		this.isAvailable = isAvailable;
-		System.out.println("Product id: "+ this.id);
-		System.out.println("Product name: " +this.name);
-		System.out.println("Product Catergory: " + this.category);
-		System.out.println("Product rating: " + this.rating + "/5.0");
-		System.out.println("Product in stock: " + this.isAvailable);
-		System.out.println(this.discountPercentage + "% discount available");
-		System.out.println("Originial Product Price: £" +this.price);
-		System.out.println("Discounted Product Price: £" + (this.price - (this.price * this.discountPercentage/100)));
-		System.out.println();
+		
 	}
 	
 }
